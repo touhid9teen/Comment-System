23e0cf5e2615aa55743032567b7df46acc687b7d
touhid9teen <thisis9teen@gmail.com>
2026-01-17 14:55:40 +0600

    feat: Implement a new reusable Modal component with basic functionality and styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 
 import { Avatar } from './components/ui/Avatar'
-
+import { Modal } from './components/ui/Modal'
+import { useState } from 'react'
 function App() {
  
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <>
       <Avatar src="https://placehold.co/64x64" alt="A" />
+      <button onClick={() => setIsOpen(true)}>Open Modal</button>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} title="Modal">
+        <p>Modal content</p>
+      </Modal>
     </>
-  )
+  ) 
 }
 
 export default App
