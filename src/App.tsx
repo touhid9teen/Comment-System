3b66c8f3aa1282712ebc23a2289a964d638b1d79
touhid9teen <thisis9teen@gmail.com>
2026-01-18 02:18:54 +0600

    feat: Refactor App component structure to include routing and authentication context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Header } from "./components/layout/Header";
+import { AuthProvider } from "./context/AuthContext";
+import { Home } from "./pages/Home";
+import { AuthCallback } from "./pages/AuthCallback";
 
-import { Home } from './pages/Home'
 function App() {
- 
   return (
-    <>
-      <Home/>
-    </>
-  ) 
+    <AuthProvider>
+      <Router>
+        <div className="app-layout">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/auth/callback" element={<AuthCallback />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/main.scss";
 import App from "./App.tsx";
-import { AuthProvider } from "./context/AuthContext";
+import "./styles/main.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </StrictMode>,
 );
